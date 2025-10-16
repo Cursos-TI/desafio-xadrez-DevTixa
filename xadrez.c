e77d832db5dce3d1a51f2876ac50edb3d4d34f1b
DevTixa <devtixa@gmail.com>
2025-10-16 02:57:05 +0000

    1.1v

--- xadrez.c
+++ xadrez.c
@@ -1,49 +1,70 @@
 #include <stdio.h>
 
+// Movimento da Torre
+void moverTorre(int casas){
+    if (casas > 0)
+    {
+        printf("Direita\n");
+        moverTorre(casas - 1);
+    }
+    
+}
+
+// Movimento do Bispo
+void moverBispo(int casas, int passoVertical, int passoHorizontal){
+    if (casas > 0)
+    {
+        for (int i = 0; i < passoVertical; i++)
+        {
+            printf("Cima\n");
+        }
+        
+        for (int j = 0; j < passoHorizontal; j++)
+        {
+            printf("Direita\n");
+        }
+
+        moverBispo(casas - 1, passoVertical, passoHorizontal);
+    }
+}
+
+// Movimento da Rainha
+void moverRainha(int casas){
+    if (casas > 0)
+    {
+        printf("Esquerda\n");
+        moverRainha(casas - 1);
+    }
+    
+}
+
+// Movimento do Cavalo
+void moverCavalo(int movimentos){
+    for (int m = 0; m < movimentos; m++) // Loop externo: cada "L"
+    {
+        // Loop interno para mover vertical (2 casas para cima)
+        for (int v = 0; v < 2; v++)
+        {
+            printf("Cima\n");
+        }
+
+        // Loop interno para mover horizontal (1 casa para direita)
+        for (int h = 0; h < 1; h++)
+        {
+            printf("Direita\n");
+        }
+    }
+}
+
 int main() {
 
-    // Torre: FOR = 5 casas para frente
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Direita\n"); // Imprime a direção do movimento
-    }
-
+    moverTorre(5);
     printf("\n");
-    
-    // Bispo: While = 5 casas na diagonal
-    int i1 = 0;
-
-    while (i1 < 5)
-    {
-        printf("Cima, Direita\n"); // Imprime a direção do movimento
-        i1++;
-    }
-
+    moverBispo(5, 1, 1);
     printf("\n");
-    
-    // Rainha: Do-While = 8 casas para esquerda
-    int i2 = 0;
-
-    do
-    {
-        printf("Esquerda\n"); // Imprime a direção do movimento
-        i2++;
-
-    } while (i2 < 8);
-
+    moverRainha(8);
     printf("\n");
+    moverCavalo(1);
 
-    // Cavalo: Loop Aninhado = 2 casas para baixo e 1 casa para a esquerda
-    int i = 1;
-
-    while (i--)
-    {
-        for (int i = 0; i < 2; i++)
-        {
-            printf("Baixo\n"); // Imprime "Baixo" duas vezes
-        }
-        printf("Esquerda\n"); //Imprime "Esquerda" duas vezes    
-    }
-     
     return 0;
 }
